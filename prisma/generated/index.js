62f0e0e7299678d935270241003eb21899d13f24
Dakar Fleet <gueda2011@gmail.com>
2026-03-22 09:48:23 +0000

    Regenerate Prisma client

--- prisma/generated/index.js
+++ prisma/generated/index.js
@@ -812,7 +812,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "..",
--- prisma/generated/wasm.js
+++ prisma/generated/wasm.js
@@ -811,7 +811,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "..",
